ce16df96d0b8d9a9c015b7edfcc734318ea54b30
Ekampreet Kaur <ekaur@mail.greenriver.edu>
2020-01-06 14:45:26 -0500

    created new index file

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/** Ekampreet Kaur
+ * 1/6/2020
+ * http://ekaur.greenriverdev.com/328/pp1/index.php
+ * Pair Program 1
+ */
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<!DOCTYPE html>
+<body lang="en">
+<head>
+<body>
+<meta charset="UTF-8">
+<title>Pair Program 1</title>
+<h1>Pair Program 1 </h1>
+
